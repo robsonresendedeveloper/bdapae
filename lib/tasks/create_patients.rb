ea090eae882ff0d86204987525ff7bbf021cf3d4
Robson Resende <robson.souza.cl@gmail.com>
2019-05-13 18:03:30 -0300

    Creating patients of real world

---
+++ lib/tasks/create_patients.rb
@@ -0,0 +1,20 @@
+@patient = Patient.new(name: "Aluísio Jorge Neves Andrade", sex: "Masculino",  entry_date: "1968-01-01")
+@patient.save
+@patient = Patient.new(name: "Amarilis Gonçalves Sousa", sex: "Feminino",  entry_date: "1968-01-01")
+@patient.save
+@patient = Patient.new(name: "Angela Bernadete Pôssas", sex: "Feminino",  entry_date: "1968-01-01")
+@patient.save
+@patient = Patient.new(name: "Amaury Messias ", sex: "Masculino")
+@patient.save
+@patient = Patient.new(name: "Altamir José do Nascimento", sex: "Masculino",  entry_date: "1971-01-01")
+@patient.save
+@patient = Patient.new(name: "Arlindo Franscisco Andrade", sex: "Masculino")
+@patient.save
+@patient = Patient.new(name: "Alcebíades Pereira Filho", sex: "Masculino")
+@patient.save
+@patient = Patient.new(name: "Antônio Mariano da Silva", sex: "Masculino")
+@patient.save
+@patient = Patient.new(name: "Agenor Felipe Rosa", sex: "Masculino",  entry_date: "1968-01-01")
+@patient.save
+@patient = Patient.new(name: "Altair José Pereira", sex: "Masculino",  entry_date: "1969-01-01")
+@patient.save
